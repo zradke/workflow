fb75e2e564401f4f982675ee5931d14b53364cef
Zach Klippenstein <zach.klippenstein@gmail.com>
2020-02-22 18:12:02 -0800

    Introduce ComposeWorkflow, a self-rendering Workflow.

---
+++ kotlin/samples/hello-compose-rendering/src/main/java/com/squareup/sample/hellocomposerendering/HelloRenderingWorkflow.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.sample.hellocomposerendering
+
+import androidx.compose.Composable
+import androidx.ui.core.Text
+import androidx.ui.foundation.Clickable
+import androidx.ui.layout.Center
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.ripple.Ripple
+import androidx.ui.material.surface.Surface
+import androidx.ui.tooling.preview.Preview
+import com.squareup.sample.hellocomposerendering.HelloRenderingWorkflow.Toggle
+import com.squareup.workflow.Sink
+import com.squareup.workflow.compose.ComposeWorkflow
+import com.squareup.workflow.ui.ContainerHints
+import com.squareup.workflow.ui.compose.tooling.preview
+
+/**
+ * A [ComposeWorkflow] that is used by [HelloWorkflow] to render the screen.
+ */
+object HelloRenderingWorkflow : ComposeWorkflow<String, Toggle>() {
+
+  object Toggle
+
+  @Composable override fun render(
+    props: String,
+    outputSink: Sink<Toggle>,
+    containerHints: ContainerHints
+  ) {
+    MaterialTheme {
+      Ripple(bounded = true) {
+        Clickable(onClick = { outputSink.send(Toggle) }) {
+          Center {
+            Text(props)
+          }
+        }
+      }
+    }
+  }
+}
+
+@Preview(heightDp = 150)
+@Composable private fun HelloBindingPreview() {
+  Surface {
+    HelloRenderingWorkflow.preview("Hello")
+  }
+}
